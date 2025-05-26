c9ec95874d187c1218d0e3d98735253742537628
NadimPy <nadimchendy2@gmail.com>
2025-05-27 01:43:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 gsap.to(".title", \{\
+  duration: 1,\
+  opacity: 1,\
+  y: 0,\
+  ease: "power2.out"\
+\});\
+\
+gsap.to(".subtitle", \{\
+  duration: 1,\
+  delay: 0.5,\
+  opacity: 1,\
+  y: 0,\
+  ease: "power2.out"\
+\});\
+}
